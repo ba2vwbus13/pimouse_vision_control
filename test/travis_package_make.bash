71fca0096b832af77643b399902318a5c60362ee
Katsuya Nakahira <ba2vwbus13wind@gmail.com>
2019-08-14 08:08:59 +0000

    add test

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -xve
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 18DAAE7AECA3745F
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys  6B05F25D762E3157
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 sudo apt-get update
